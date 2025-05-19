552bc59cafe38d4097282c8df4834ccde1a50362
ntua-el21090 <el21090@mail.ntua.gr>
2025-05-19 17:31:40 +0200

    baseline model corrections

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -9,20 +9,20 @@ class CRNN(nn.Module):
     def __init__(self, n_classes):
         super(CRNN, self).__init__()
         resnet = models.resnet18(pretrained=True)
-        self.cnn = nn.Sequential(*list(resnet.children())[:-3])  # output: [B, 256, H/8, W/8]
+        self.cnn = nn.Sequential(*list(resnet.children())[:-3])  
 
         self.rnn = nn.LSTM(512, 256, bidirectional=True, num_layers=2, batch_first=True)
         self.fc = nn.Linear(512, n_classes)
 
     def forward(self, x):
         x = self.cnn(x)
-        print("After CNN:", x.shape)  # Debug: [B, C, H, W]
+        print("After CNN:", x.shape)  
 
         b, c, h, w = x.size()
         x = x.permute(0, 3, 1, 2).contiguous()
         x = x.view(b, w, c * h)
-        print("After reshape for RNN:", x.shape)  # Debug: [B, W, C*H]
+        print("After reshape for RNN:", x.shape)  
 
         x, _ = self.rnn(x)
         x = self.fc(x)
-        return x.permute(1, 0, 2)  # [T, B, C] for CTC loss
+        return x.permute(1, 0, 2) 
